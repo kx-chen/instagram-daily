9110da1a3427f75c71e9529fe94d74ffc34d7581
Kai Chen <kaichen120@gmail.com>
2018-04-14 19:59:57 -0700

    Implemented posting onion photo with webhook.

--- webhook.py
+++ webhook.py
@@ -1,16 +1,34 @@
-# instapy -u kaiusesthis -p password899 -f https://instagram.fcxh3-1.fna.fbcdn.net/vp/c5ef6cab120b908d85657dfca4966d25/5B6DBF75/t51.2885-15/e35/30605196_182396209053880_8553512260012605440_n.jpg?ig_cache_key=MTc1NzU0MjM5MjkwMTQ1NDIxOQ%3D%3D.2  -t 'staging account'
-
 from flask import Flask, request, abort
-import os 
-import pynstagram
+import os, datetime
+from InstagramAPI import InstagramAPI
 
 app = Flask(__name__)
 dir_path = os.path.dirname(os.path.realpath(__file__))
+quotes = [
+	"An onion can make people cry but there's never been a vegetable that can make people laugh.",
+	"All we have to do is to peel the shrines like an onion, and we will be with the king himself.",
+	"If you dig deep and keep peeling the onion, artists and freelance writers are the leaders in society - the people who start to get new ideas out.",
+	"Good intentions are not enough. They've never put an onion in the soup yet.",
+	"Life is like an onion. You peel it off one layer at a time, and sometimes you weep.",
+	"It doesn't matter how precisely the onion is cut as long as the person chewing it is happy.",
+	"Opinions are like onions. They spell similarly, usually have many layers, and tend to make people cry."
+]
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/daily_post', methods=['GET'])
 def webhook():
-    os.system("""instapy -u kaiusesthis -p password899 -f https://instagram.fcxh3-1.fna.fbcdn.net/vp/c5ef6cab120b908d85657dfca4966d25/5B6DBF75/t51.2885-15/e35/30605196_182396209053880_8553512260012605440_n.jpg?ig_cache_key=MTc1NzU0MjM5MjkwMTQ1NDIxOQ%3D%3D.2  -t 'staging account' """)
-    return 'success'
+    Instagram = InstagramAPI(os.environ['username'], os.environ['password'])
+    Instagram.login()  # login
+
+    day_number = datetime.date.today().strftime("%w")
+    photo_path = dir_path + '/onion.jpg'
+    caption = quotes[int(day_number)] + " #dailyonion"
+    Instagram.uploadPhoto(photo_path, caption=caption)
+    return 'Sucessfully posted.'
+
+@app.route("/", methods=['GET'])
+def index():
+    return '<a href="/daily_post"> daily post </a>'
 
 if __name__ == '__main__':
     app.run()
+    
